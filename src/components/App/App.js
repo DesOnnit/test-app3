389dc1eee0ca83f18f1408d6174d4a9c0a03c779
Vadim <vadik.kortkov@yandex.ru>
2022-04-07 15:11:42 +0300

    add popup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../NavBar/NavBar';
 import Header from '../Header/Header';
 import Services from '../Services/Services';
@@ -9,23 +9,30 @@ import Industries from '../Industries/Industries';
 import './App.css';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  function BurgerOpen() {
+    setIsOpen(true);
+  }
 
-  const [isVisitable,setIsVisitable] = useState(true);
+  const [isVisitable, setIsVisitable] = useState(true);
   useEffect(() => {
     window.addEventListener(`resize`, () => {
       let size = window.innerWidth
-      size <=1750? setIsVisitable(false):setIsVisitable(true);
-    })},[])
+      size <= 1750 ? setIsVisitable(false) : setIsVisitable(true);
+    })
+  }, [])
   return (
     <div className="App">
       <NavBar
-      hiden={isVisitable}/>
-      <Header/>
-      <Services/> 
-      <Industries/>
-      <Team/>
-      <TechnologyMap/> 
-      <Contacts/>  
+        hiden={isVisitable}
+        open={BurgerOpen}
+        isOpen={isOpen} />
+      <Header />
+      <Services />
+      <Industries />
+      <Team />
+      <TechnologyMap />
+      <Contacts />
     </div>
   );
 }
--- src/components/NavBarMenu/NavBarMenu.js
+++ src/components/NavBarMenu/NavBarMenu.js
@@ -2,7 +2,7 @@ import './NavBarMenu.css'
 export default function NavBarMenu(props){
     return(
         <>
-        <a href={props.link.src} className='navbar-menu__link'>{props.link.title}<span className='navbar-menu__link_dot'/></a>
+        <a href={props.link.src} className={!props.isOpen? 'navbar-menu__link':'navbar-menu__link open'}>{props.link.title}<span className='navbar-menu__link_dot'/></a>
         </>
     )
 }
